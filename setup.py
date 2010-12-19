9436c3bd9306637ea2f04258b1a5f3fa3fa88e80
Gabriel Grant <g@briel.ca>
2010-12-19 10:25:21 -0500

    added an author field an PIL dependency to setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,18 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='django-articles',
     version='0.1dev',
+    author='Gabriel Grant',
     packages=['articles',],
     license='LGPL',
     long_description=open('README').read(),
+    install_requires=[
+        'PIL',
+    #    'django-html-field',
+    ],
+    dependency_links = [
+    #    'http://github.com/gabrielgrant/django-html-field/tarball/master#egg=django-html-field',
+    ]
 )
 
