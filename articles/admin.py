8ca1105c2fb0592aac3018fa5164435fe283d696
Gabriel Grant <g@briel.ca>
2010-12-18 23:45:17 -0500

    added an admin.py and a setup.py

---
+++ articles/admin.py
@@ -0,0 +1,7 @@
+from articles.models import Article, ArticleImage
+from django.contrib import admin
+
+admin.site.register(Article)
+admin.site.register(ArticleImage)
+
+
---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='django-articles',
+    version='0.1dev',
+    packages=['articles',],
+    license='LGPL',
+    long_description=open('README').read(),
+)
+
