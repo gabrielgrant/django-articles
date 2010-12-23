5564c5c1c264adb9504c8d26a32499263bf6997d
Gabriel Grant <g@briel.ca>
2010-12-23 15:11:23 -0500

    bumped version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='django-articles',
-    version='0.1.1dev',
+    version='0.1.2dev',
     author='Gabriel Grant',
     packages=['articles',],
     license='LGPL',
