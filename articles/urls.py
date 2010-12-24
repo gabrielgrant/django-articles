407c8ffb9203051c05aba97e38039667f19d3c98
Gabriel Grant <g@briel.ca>
2010-12-24 00:33:11 -0500

    added urls.py

---
+++ articles/urls.py
@@ -0,0 +1,39 @@
+from django.conf.urls.defaults import *
+from django.views.generic.dates import DateDetailView, DayArchiveView,  \
+	MonthArchiveView, YearArchiveView, ArchiveIndexView
+from articles.views import ArticleDateView
+from articles.models import Article
+
+
+def get_patterns(info_dict, basename):
+	urlpatterns = patterns('',
+		url(
+			r'^(?P<year>\d{4})/(?P<month>[a-z]{3})/(?P<day>\w{1,2})/(?P<slug>[\w-]+)/$',
+			ArticleDateView.as_view(**dict(info_dict, slug_field='slug')),
+			name='%s_detail' % basename
+		),
+		url(r'^(?P<year>\d{4})/(?P<month>[a-z]{3})/(?P<day>\w{1,2})/$',
+			DayArchiveView.as_view(**info_dict),
+			name='%s_day' % basename
+		),
+		url(r'^(?P<year>\d{4})/(?P<month>[a-z]{3})/$',
+			MonthArchiveView.as_view(**info_dict),
+			name='%s_month' % basename
+		),
+		url(r'^(?P<year>\d{4})/$',
+			YearArchiveView.as_view(**info_dict),
+			name='%s_year' % basename
+		),
+		url(r'^/?$',
+			ArchiveIndexView.as_view(**info_dict),
+			name='%s_index' % basename
+		),
+	)
+	return urlpatterns
+
+
+base_info_dict = {
+	'queryset': Article.objects.filter(published=True),
+	'date_field': 'pub_date',
+}
+urlpatterns = get_patterns(base_info_dict, 'article')
