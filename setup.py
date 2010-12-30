6424ac6353e42843504b3ec042df68f250716639
Gabriel Grant <g@briel.ca>
2010-12-30 14:53:41 -0500

    add missing django-html-field dependency

--- setup.py
+++ setup.py
@@ -12,13 +12,13 @@ setup(
         'django-inline-edit',
         'django-ckeditor',
         'django-ckeditor-filemodel-manager',
-    #    'django-html-field',
+        'django-html-field',
     ],
     dependency_links = [
     	'http://github.com/gabrielgrant/django-inline-edit/tarball/master#egg=django-inline-edit'
     	'http://github.com/gabrielgrant/django-ckeditor/tarball/master#egg=django-ckeditor'
     	'http://github.com/gabrielgrant/django-ckeditor-filemodel-manager/tarball/master#egg=django-ckeditor-filemodel-manager'
-    #    'http://github.com/gabrielgrant/django-html-field/tarball/master#egg=django-html-field',
+        'http://github.com/gabrielgrant/django-html-field/tarball/master#egg=django-html-field',
     ]
 )
 
