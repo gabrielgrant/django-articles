34554dc5d67bab15015b6c1c067dde0de9651cef
Gabriel Grant <g@briel.ca>
2010-12-30 14:44:13 -0500

    use django-ckeditor (by providing admin helpers); bump version number

--- articles/admin.py
+++ articles/admin.py
@@ -1,7 +1,37 @@
 from articles.models import Article, ArticleImage
 from django.contrib import admin
+from django import forms
+from ckeditor.widgets import CKEditor
+from html_field.forms.widget_helper import make_toolbar_config
 
-admin.site.register(Article)
+def article_admin(article_model):
+	base_allow_tags = ['a','h2','em','strong','ol','ul']
+	class ArticleAdminForm(forms.ModelForm):
+		class Meta:
+			model = article_model
+			widgets = {
+				'blurb': CKEditor(
+					ckeditor_config=dict(width='750',
+						**make_toolbar_config(
+							allow_tags=base_allow_tags,
+						)
+					)
+				),
+				'body': CKEditor(
+					ckeditor_config=dict(width='750',
+						**make_toolbar_config(
+							allow_tags=base_allow_tags + ['img'],
+						)
+					)
+				)
+			}
+
+	class ArticleAdmin(admin.ModelAdmin):
+		form = ArticleAdminForm
+	
+	return ArticleAdmin
+
+admin.site.register(Article, article_admin(Article))
 admin.site.register(ArticleImage)
 
 
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='django-articles',
-    version='0.1.4dev',
+    version='0.1.5dev',
     author='Gabriel Grant',
     packages=['articles',],
     license='LGPL',
@@ -10,10 +10,12 @@ setup(
     install_requires=[
         'PIL',
         'django-inline-edit',
+        'django-ckeditor',
     #    'django-html-field',
     ],
     dependency_links = [
     	'http://github.com/gabrielgrant/django-inline-edit/tarball/master#egg=django-inline-edit'
+    	'http://github.com/gabrielgrant/django-ckeditor/tarball/master#egg=django-ckeditor'
     #    'http://github.com/gabrielgrant/django-html-field/tarball/master#egg=django-html-field',
     ]
 )
